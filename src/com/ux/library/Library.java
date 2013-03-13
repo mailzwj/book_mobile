076a313bffcd04ceb791861388fb57e1b33f3b3e
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2013-03-13 20:01:46 +0800

    新增关闭应用功能

--- src/com/ux/library/Library.java
+++ src/com/ux/library/Library.java
@@ -3,7 +3,9 @@ package com.ux.library;
 import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Library extends DroidGap {
 
@@ -20,5 +22,17 @@ public class Library extends DroidGap {
 		getMenuInflater().inflate(R.menu.library, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
 
+		Log.v("item", Integer.toString(item.getItemId()));
+		switch(item.getItemId()){
+			case R.id.closeapp: {
+				finish();
+				break;
+			}
+		}
+		return false;
+	}
 }
