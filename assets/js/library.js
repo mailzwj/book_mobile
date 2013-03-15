9a196f906f38c9b94f08b36585c32bc957232dd1
lingwu.tyf@taobao.com <lingwu.tyf@taobao.com>
2013-03-15 11:37:59 +0800

    complete mobile login

--- assets/js/library.js
+++ assets/js/library.js
@@ -1,13 +1,10 @@
 ﻿$(document).bind("pageinit", function(){
-	$.sessionStorage.setItem("host", "http://10.13.124.223:8080/book");
+	$.sessionStorage.setItem("host", "http://10.13.124.203:8080/book");
 	$("#App-submit").click(function(e){
-		var un = $("#App-user").val();
-		var up = $("#App-pass").val();
 		var host = $.sessionStorage.getItem("host");
-		var login = host + "/login?callback=?&username=" + un + "&password=" + up;
-
-		$.getJSON(login + "&t=" + new Date().getTime(), function(data){
-		    //alert(JSON.stringify(data));
+		var login = host + "/login";
+		
+		$.post(login, $('#loginForm').serialize(), function(data) {			
 			if(data.isSuccess){
 				$.sessionStorage.setItem("userinfo", JSON.stringify(data.userinfo));
 				window.location.href = "list.html";
@@ -15,7 +12,8 @@
 			}else{
 				alert("登录失败，请检查用户名和密码。");
 			}
-		});
+		}, 'json');
+		
 		e.preventDefault();
 	});
 });
