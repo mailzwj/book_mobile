3cfe3ef1f59f5a81e1e8f8583741c345ef4002d6
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2013-03-26 14:54:15 +0800

    调整登录接口

--- assets/js/library.js
+++ assets/js/library.js
@@ -160,14 +160,15 @@ _detailBook.detail.prototype = {
 var currentisbn = "";
 
 $(document).bind("pageinit", function(){
-	$.sessionStorage.setItem("host", "http://10.13.124.201:8080/book");
+	$.sessionStorage.setItem("host", "http://10.13.124.199:8080/book");
 	//点击登录
 	$("#App-submit").unbind("click");
 	$("#App-submit").click(function(e){
 		var host = $.sessionStorage.getItem("host");
 		var login = host + "/login";
 		
-		$.post(login, $('#loginForm').serialize(), function(data) {			
+		$.post(login, $('#loginForm').serialize(), function(data) {
+		    alert(JSON.stringify(data));		
 			if(data.isSuccess){
 				$.sessionStorage.setItem("userinfo", JSON.stringify(data.userinfo));
 				//window.location.href = "list.html";
